c015e2bbedc29fab6fbde7978f46377254b9d5e9
Adam Heath <doogie@brainfood.com>
2019-06-07 13:24:07 -0500

    WIP: moqui/nextcloud connector.

--- service/bf-team/Invite.groovy
+++ service/bf-team/Invite.groovy
@@ -21,13 +21,34 @@ import org.moqui.entity.EntityCondition
 import org.moqui.entity.EntityFind
 import org.moqui.entity.EntityList
 import org.moqui.entity.EntityValue
-
+import org.moqui.resource.ResourceReference
 
 @Field Logger logger = LoggerFactory.getLogger('Invite')
+@Field Timestamp now = context.ec.user.nowTimestamp
+
+private EntityFind createDynamicView() {
+    logger.info('createDynamicView')
+    EntityFind ef = ec.entity.find('WorkEffort')
+    logger.info('ef=' + ef)
+    ef.makeEntityDynamicView()
+        .addMemberEntity('we', 'WorkEffort', null, null, null)
+        .addMemberEntity('wecm', 'WorkEffortContactMech', 'we', false, [workEffortId: 'workEffortId'])
+        .addMemberEntity('cm', 'ContactMech', 'wecm', false, [contactMechId: 'contactMechId'])
+        .addAlias('we', 'workEffortId')
+        .addAlias('we', 'workEffortName')
+        .addAlias('we', 'statusId')
+        .addAlias('wecm', 'fromDate')
+        .addAlias('wecm', 'thruDate')
+        .addAlias('wecm', 'contactMechPurposeId')
+        .addAlias('cm', 'infoString')
+        .addAlias('cm', 'contactMechTypeEnumId')
+    logger.info('ef=' + ef)
+    return ef
+}
 
 // api
-void createInvite() {
-    logger.info('createInvite')
+Map<String, Object> inviteUser() {
+    logger.info('inviteUser')
     ExecutionContext ec = context.ec
 
     String httpHostname = context.httpHostname
@@ -36,8 +57,145 @@ void createInvite() {
     String lastName = context.lastName
     String emailAddress = context.emailAddress
 
-    workEffortId = 'foo'
-    currentStatusId = 'bar'
+    EntityFind ef = createDynamicView()
+    ef
+        .condition([
+            workEffortName: 'TEAM_MEMBER_INVITE',
+            workEffortTypeEnumId: 'BfTeamInvite',
+            //statusId: 'WeInProgress',
+            contactMechPurposeId: 'EmailPrimary',
+            contactMechTypeEnumId: 'CmtEmailAddress',
+            infoString: emailAddress,
+        ])
+        .conditionDate('fromDate', 'thruDate', now)
+
+    EntityList workEfforts = ef.list()
+    String workEffortId
+    if (workEfforts.isEmpty()) {
+        Map<String, Object> cweResult = ec.service.sync().name('create', 'mantle.work.effort.WorkEffort').parameters([
+            workEffortName: 'TEAM_MEMBER_INVITE',
+            workEffortTypeEnumId: 'BfTeamInvite',
+            statusId: 'WeInProgress',
+        ]).call()
+        workEffortId = cweResult.workEffortId
+        Map<String, Object> ceaResult = ec.service.sync().name('mantle.party.ContactServices.create#EmailAddress').parameters([
+            emailAddress: emailAddress,
+        ]).call()
+        String contactMechId = ceaResult.contactMechId
+        ec.service.sync().name('create', 'mantle.work.effort.WorkEffortContactMech').parameters([
+			workEffortId: workEffortId,
+            contactMechId: contactMechId,
+            contactMechPurposeId: 'EmailPrimary',
+        ]).call()
+        ResourceReference jsonDataRef = ec.resource.getLocationReference('dbresource://bf-team/work/invite/' + workEffortId + '.json')
+        jsonDataRef.putText(JsonOutput.toJson([
+            httpHostname: httpHostname,
+            httpScheme: httpScheme,
+            newUserEmailAddress: emailAddress,
+            newUserFirstName: firstName,
+            newUserLastName: lastName,
+        ]))
+        ec.service.sync().name('create', 'mantle.work.effort.WorkEffortContent').parameters([
+			workEffortId: workEffortId,
+			contentLocation: jsonDataRef.getLocation(),
+            contentTypeEnumId: 'BfTeamInviteMetaData',
+        ]).call()
+    } else {
+        workEffortId = workEfforts[0].workEffortId
+    }
+/*
+    emailAddress -> WorkEffortContactMech
+
+    attributes -> WorkEffortContent
+        contentTypeEnumId = WorkEffortAttribute
+        contentLocation = $name
+        description = $value
+*/
+    return [
+        workEffortId: workEffortId,
+        //statusId: 'PENDING',
+    ]
+}
+
+// This is run via eeca on WorkEffortContactMech[EmailPrimary]
+Map<String, Object> checkExistingUser() {
+    logger.info('checkExistingUser')
+    ExecutionContext ec = context.ec
+
+    String partyId = context.partyId
+    String workEffortId = context.workEffortId
+
+    EntityFind ef = createDynamicView()
+    ef.condition([
+        workEffortId: workEffortId,
+        workEffortName: 'TEAM_MEMBER_INVITE',
+        workEffortTypeId: 'BfTeamInvite',
+        //statusId: 'WeInProgress',
+
+        contactMechPurposeId: 'EmailPrimary',
+        contactMechTypeEnumId: 'EmailAddress',
+    ])
+    .conditionDate('fromDate', 'thruDate', now)
+
+    EntityValue wecm = ef.list().getFirst()
+    logger.info('wecm=' + wecm)
+    if (wecm == null) {
+        return [:]
+    }
+
+    String emailAddress = wecm.infoString
+
+
+    // FIXME: study UserAccount, perhaps use bf-auth-moqui instead
+    /*
+    EntityValue userLogin = delegator.findOne('UserLogin', [userLoginId: emailAddress.toLowerCase()], false)
+    String partyId = userLogin?.partyId
+    if (partyId) {
+        dispatcher.runSync('updateWorkEffort', [
+            userLogin: context.userLogin,
+            workEffortId: workEffortId,
+            currentStatusId: 'PARTYINV_ACCEPTED',
+        ])
+        dispatcher.runSync('assignPartyToWorkEffort', [
+            userLogin: context.userLogin,
+            workEffortId: weaa.workEffortId,
+            partyId: partyId,
+            roleTypeId: 'PERSON_ROLE',
+            statusId: 'PARTY_ENABLED',
+        ])
+    }
+    */
+    return [:]
+}
+
+Map<String, Object> attachInviteToOwnerTeam() {
+    logger.info('attachInviteToOwnerTeam')
+    ExecutionContext ec = context.ec
+
+    String partyId = context.partyId
+    String workEffortId = context.workEffortId
+
+    logger.info('partyId=' + partyId + ', workEffortId=' + workEffortId)
+    if (!partyId) {
+        return [:]
+    }
+
+    EntityList existingParties = ec.entity.find('mantle.work.effort.WorkEffortParty').condition([
+        workEffortId: workEffortId,
+        partyId: partyId,
+        roleTypeId: 'Owner',
+    ]).conditionDate('fromDate', 'thruDate', now).list()
+    if (existingParties.isEmpty()) {
+        ec.service.sync().name('create', 'mantle.work.effort.WorkEffortParty').parameters([
+            workEffortId: workEffortId,
+            partyId: partyId,
+            roleTypeId: 'Owner',
+            fromDate: now,
+            statusId: 'WeptAssigned',
+        ]).call()
+    }
+
+    return [:]
 }
 
 // api
--- service/bf-team/Team.groovy
+++ service/bf-team/Team.groovy
@@ -24,40 +24,234 @@ import org.moqui.entity.EntityValue
 
 
 @Field Logger logger = LoggerFactory.getLogger('Team')
+@Field Timestamp now = context.ec.user.nowTimestamp
+//@Field String currentUserPartyId = context.partyId || ec.user.userAccount?.partyId || 'alyvr-admin'
+@Field String currentUserPartyId = 'alyvr-admin'
 
-// api
-void readTeam() {
-    logger.info('readTeam')
+void ensureTeamMemberRoles() {
     ExecutionContext ec = context.ec
-    String teamPartyId = context.teamPartyId
-
-    long now = System.currentTimeMillis()
-    List<Object> payload = [now, emailAddress, partyId]
-    String payloadJson = JsonOutput.toJson(payload)
-
-    teamName = 'Mock Team'
-    owner = [
-        foo: 'bar',
-    ]
-    members = [
-        [foo: 'bar'],
-    ]
-    teamPartyRoles = ['role1', 'role2']
-}
-
-// api
-void updateTeamMember() {
-    logger.info('updateTeamMember')
-
-    String teamPartyId = context.teamPartyId
     String memberPartyId = context.memberPartyId
     Collection<String> roleTypes = context.roleTypes
+    for (String roleTypeId: roleTypes) {
+        ec.service.sync().name('ensure', 'PartyRole').parameters([partyId: memberPartyId, roleTypeId: roleTypeId]).call()
+    }
 }
 
-// api
-void deleteTeamMember() {
-    logger.info('deleteTeamMember')
+/*
+Team[PartyGroup]
+PartyRelationship
+    relationshipTypeEnumId = 'PrtMember'
+    fromPartyId = Team[PartyGroup]
+    fromRoleTypeId = 'OrgTeam'
+    toPartyId = Member[Party*]
+    toRoleTypeId = ''
+        'Owner'
+        'Administrator'
+        'Manager'
+        'Reporter'
+        'Subscriber'
+        'Contractor'
+    statusId = ''
+*/ 
 
-    String teamPartyId = context.teamPartyId
+// internal
+public Map<String, Object> createTeam() {
+    logger.info('createTeam')
+    ExecutionContext ec = context.ec
+    String teamName = context.teamName
+    String ownerPartyId = context.ownerPartyId
+
+    EntityValue teamParty = ec.entity.makeValue('mantle.party.Party', [partyTypeEnumId: 'PtyOrganization']).setSequencedIdPrimary().create()
+    String teamPartyId = teamParty.partyId
+    ec.entity.makeValue('mantle.party.PartyRole', [partyId: teamPartyId, roleTypeId: 'OrgTeam']).create()
+    ec.service.sync().name('ensure', 'PartyRole').parameters([partyId: ownerPartyId, , roleTypeId: 'Owner']).call()
+    ec.entity.makeValue('mantle.party.PartyRelationship', [
+        relationshipTypeEnumId: 'PrtMember',
+        fromPartyId: teamPartyId,
+        fromRoleTypeId: 'OrgTeam',
+        toPartyId: ownerPartyId,
+        toRoleTypeId: 'Owner',
+        fromDate: now,
+    ]).setSequencedIdPrimary().create()
+    return [partyId: teamPartyId]
+    // TODO?: Add a per-install ECA to attach the team to the top-level organization
+/*
+    PtyOrganization
+    <mantle.party.Party partyId="ORG_ZIZI_HR" pseudoId="ZIHR" partyTypeEnumId="PtyOrganization" ownerPartyId="ORG_ZIZI_HR"/>
+    <mantle.party.Organization partyId="ORG_ZIZI_HR" organizationName="Ziziwork Human Resources"/>
+    <mantle.party.PartyRole partyId="ORG_ZIZI_HR" roleTypeId="OrgInternal"/>
+    <mantle.party.PartyRelationship partyRelationshipId="ORG_ZIZI_HR" relationshipTypeEnumId="PrtOrgRollup"
+            fromPartyId="ORG_ZIZI_HR" fromRoleTypeId="OrgInternal" toPartyId="ORG_ZIZI_CORP" toRoleTypeId="OrgInternal"
+            fromDate="1265184000000"/>
+*/
+}
+
+protected void setRoleDescriptions(Map<String, Map<String, String>> roleTypeDescriptions) {
+    ExecutionContext ec = context.ec
+    for (Map.Entry<String, Map<String, String>> roleDescEntry: roleTypeDescriptions.entrySet()) {
+        EntityValue roleType = ec.entity.find('mantle.party.RoleType').condition('roleTypeId', roleDescEntry.getKey()).one()
+        roleDescEntry.value = [roleTypeId: roleType.roleTypeId, description: roleType.description]
+    }
+}
+
+protected List<Map<String, Object>> readTeamPartyRoles(String teamPartyId, String optionalMemberPartyId, boolean resolve) {
+    ExecutionContext ec = context.ec
+
+    Map<String, String> searchContext = [
+        relationshipTypeEnumId: 'PrtMember',
+        fromPartyId: teamPartyId,
+        fromRoleTypeId: 'OrgTeam',
+    ]
+    if (optionalMemberPartyId != null) {
+        searchContext.toPartyId = optionalMemberPartyId
+    }
+
+    EntityFind relations = ec.entity.find('mantle.party.PartyRelationship').condition([
+        relationshipTypeEnumId: 'PrtMember',
+        fromPartyId: teamPartyId,
+        fromRoleTypeId: 'OrgTeam',
+    ]).conditionDate('fromDate', 'thruDate', now)
+    Map<String, Set<String>> memberToRoles = [:]
+    Map<String, Map<String, String>> roleTypeDescriptions = [:]
+    for (EntityValue relation: relations.list()) {
+        String memberPartyId = relation.toPartyId
+        String memberRoleTypeId = relation.toRoleTypeId
+        if (resolve) {
+            roleTypeDescriptions[memberRoleTypeId] = null
+        }
+        Set<String> memberRoles = memberToRoles[memberPartyId]
+        if (!memberRoles) {
+            memberToRoles[memberPartyId] = memberRoles = new HashSet<>()
+        }
+        memberRoles.add(memberRoleTypeId)
+    }
+
+    if (resolve) {
+        setRoleDescriptions(roleTypeDescriptions)
+    }
+    return memberToRoles.entrySet().collect { Map.Entry<String, Set<String>> entry ->
+        return [
+            memberPartyId: entry.key,
+            roleTypes: entry.value.collect { String roleTypeId ->
+                return resolve ? roleTypeDescriptions[roleTypeId] : roleTypeId
+            }
+        ]
+    }
+}
+
+protected List<Map<String, Object>> loadTeamMembers(String teamPartyId) {
+    return readTeamPartyRoles(teamPartyId, null, true)
+}
+
+public Map<String, Object> readTeamPartyRoles() {
+    ExecutionContext ec = context.ec
+    String teamPartyId = context.partyId
+
+    List<Map<String, Object>> userPartyRoles = readTeamPartyRoles(teamPartyId, currentUserPartyId, false)
+    if (userPartyRoles) {
+        return [teamPartyRoles: userPartyRoles[0].roleTypes]
+    } else {
+        return [teamPartyRoles: []]
+    }
+}
+
+// rest api
+public Map<String, Object> readTeam() {
+    logger.info('readTeam')
+    Map<String, Object> simpleTeamResult = readSimpleTeam()
+
+    Map<String, Object> team = simpleTeamResult.team
+    team.partyTypeEnumId = 'Team'
+    team.owner = null
+    team.members = loadTeamMembers(teamPartyId)
+    return [team: team]
+}
+
+// rest api
+public Map<String, Object> readSimpleTeam() {
+    logger.info('readSimpleTeam')
+    ExecutionContext ec = context.ec
+    String teamPartyId = context.partyId
+    logger.info("teamPartyId=" + teamPartyId)
+
+    EntityValue teamOrganization = ec.entity.find('mantle.party.Organization').condition('partyId', teamPartyId).useCache(false).one()
+    EntityValue profilePicValue = ec.entity.find('mantle.party.PartyContent').condition([
+        partyId: teamPartyId,
+        partyContentTypeEnumId: 'PcntPrimaryImage',
+    ]).one()
+
+    Map<String, Object> readTeamPartyRolesResult = ec.service.sync().name('bf-team.TeamServices', 'read', 'TeamPartyRoles').parameter('partyId', teamPartyId).call()
+    return [
+        team: [
+            partyId: teamPartyId,
+            teamName: teamOrganization.organizationName,
+            profilePic: profilePicValue?.contentLocation, 
+            //owner: null,
+            userPartyRoles: readTeamPartyRolesResult.teamPartyRoles,
+        ],
+    ]
+}
+
+// rest api
+public Map<String, Object> setTeamMember() {
+    logger.info('createTeamMember')
+
+    String teamPartyId = context.partyId
     String memberPartyId = context.memberPartyId
+    Collection<String> roleTypes = context.roleTypes || []
+
+    ensureTeamMemberRoles()
+
+    // create rows
+    for (String roleTypeId: roleTypes) {
+        Map<String, Object> relRoleSearch = [
+            relationshipTypeEnumId: 'PrtMember',
+            fromPartyId: teamPartyId,
+            fromRoleTypeId: 'OrgTeam',
+            toPartyId: memberPartyId,
+            toRoleTypeId: roleTypeId,
+        ]
+        EntityFind existingFind = ec.entity.find('mantle.party.PartyRelationship').condition(relRoleSearch).conditionDate('fromDate', 'thruDate', now)
+        if (existingFind.count() == 0) {
+            ec.entity.makeValue('mantle.party.PartyRelationship', relSearch).set('fromDate', now).setSequencedIdPrimary().create()
+        }
+    }
+
+    // delete rows
+    Map<String, Object> relRemSearch = [
+        relationshipTypeEnumId: 'PrtMember',
+        fromPartyId: teamPartyId,
+        fromRoleTypeId: 'OrgTeam',
+        toPartyId: memberPartyId,
+    ]
+    EntityFind remFind = ec.entity.find('mantle.party.PartyRelationship').condition(relRoleSearch).conditionDate('fromDate', 'thruDate', now)
+    for (EntityValue remValue: remFind.list()) {
+        String toRoleTypeId = remValue.toRoleTypeId
+        if (!roleTypes.contains(remValue.toRoleTypeId)) {
+            remValue.thruDate = now
+            remValue.update()
+        }
+    }
+    return [:]
+}
+
+// rest api
+public Map<String, Object> readAllTeams() {
+    logger.info('readAllTeams')
+
+    ExecutionContext ec = context.ec
+    logger.info('-> user=' + ec.user.userAccount)
+
+    Map<String, Object> relRoleSearch = [
+        relationshipTypeEnumId: 'PrtMember',
+        fromRoleTypeId: 'OrgTeam',
+        toPartyId: currentUserPartyId,
+    ]
+    EntityFind teamFind = ec.entity.find('mantle.party.PartyRelationship').condition(relRoleSearch).conditionDate('fromDate', 'thruDate', now)
+    List<Map<String, Object>> teams = []
+    for (EntityValue teamValue: teamFind.list()) {
+        Map<String, Object> readTeamResult = ec.service.sync().name('bf-team.TeamServices', 'read', 'SimpleTeam').parameter('partyId', teamValue.fromPartyId).call()
+        teams.add(readTeamResult.team)
+	}
+	return [teams: teams]
 }
